ad78fc17a09ee3881799f1746837046341c1eea2
Łukasz Czeczko <119623998+Blazer33@users.noreply.github.com>
2024-11-30 22:30:25 +0100

    Delete czeczko_zewnetrzny.js

--- czeczko_zewnetrzny.js
+++
@@ -1,9 +0,0 @@
-document.write(
-    "ostatnia modyfikacja strony"
-        .fontcolor("#808000") 
-        .bold()              
-        .strike()            
-        .fontsize(5)         
-        + "<br>"
-);
-document.write(document.lastModified);
